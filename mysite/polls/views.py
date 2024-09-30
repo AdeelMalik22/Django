f4abc7c16badc41e532c38aefe23a3b47aae2b82
Subhan Adeel <ms1640070@gmail.com>
2024-09-30 18:11:30 +0500

    learn about url dispatcher and implement it

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,4 +5,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def function(request):
+    return HttpResponse("HI there")
+
+def add_new(request):
+    return HttpResponse("HEY HOW'S DOING")
+
 # Create your views here.
